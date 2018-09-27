fb8b4be10fd9ce934ede95133c4e07e43156ec10
anusha jain <ajain7@nd.edu>
2018-09-27 13:55:25 -0400

    New R document on Graphing Data

---
+++ Graphing.R
@@ -0,0 +1,10 @@
+##Setting Up Plot Parameters
+#par(number of rows and number of graphs per row)
+
+#par(margin space)
+?par
+
+
+##Comparitive Graphs
+#Bar graph
+#boxplot(data,graph title,x axis label, y axis label, colour)
